c9a5032fde3a313d2d045e78dbd309f43225e701
bjreddy009037 <bjreddy246@gmail.com>
2022-12-27 17:50:42 +0000

    terraform

--- main.tf
+++ main.tf
@@ -6,3 +6,16 @@ module "vpc" {
   AZ = var.AZ
   SUBNET_CIDR = var.SUBNET_CIDR
 }
+
+
+module "mysql" {
+  depends_on = [module.vpc]
+  source = "github.com/bjreddy009037/tf-module-rds-mysql"
+  DB_NAME = var.DB_NAME
+  ENGINE = var.ENGINE
+  ENGINE_VERSION = var.ENGINE_VERSION
+  SKIP_FINAL_SNAPSHOT = var.SKIP_FINAL_SNAPSHOT
+  INSTANCE_CLASS = var.INSTANCE_CLASS
+  ENV = var.ENV
+  COMPONENT = var.COMPONENT
+}
--- vars.tf
+++ vars.tf
@@ -3,3 +3,9 @@ variable "ENV" {}
 variable "COMPONENT" {}
 variable "SUBNET_CIDR" {}
 variable "AZ" {}
+
+variable "DB_NAME" {}
+variable "ENGINE" {}
+variable "ENGINE_VERSION" {}
+variable "SKIP_FINAL_SNAPSHOT" {}
+variable "INSTANCE_CLASS" {}
